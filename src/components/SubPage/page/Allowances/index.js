9371581402abd4080acb813484d3362709c0ec7b
Koras02 <wjdgh0727@gmail.com>
2021-12-31 16:42:03 +0900

    반응형 Allow페이지

--- src/components/SubPage/page/Allowances/index.js
+++ src/components/SubPage/page/Allowances/index.js
@@ -6,23 +6,20 @@ import {MdCancel} from 'react-icons/md';
 const Allow = () => {
    return (
        <div className="Allow-Container">
-           <div className="Allow-Container_body">
-              <div className="Allow-Container_body-header">
+              <div className="header">
                   <div className="header-top">
                       <h1>Manager Allowances</h1>
                       <p>View and remove allowances for all Zapper smart contracts.</p>
-                  </div>
-                  <div className="header-bottom">
                       <a href="https://learn.zapper.fi/articles/how-to-revoke-token-allowances">
                           Learn More
                       </a>
                   </div>
+                 
               </div>
-              <div className="Allow-Container_body-footer">
+              <div className="footer">
                   <div className="network">
                       <div className="container">
-                          <div className='flex'>
-                              <div className="item--active">
+                              <div className="item">
                                   <img src="https://zapper.fi/images/networks/ethereum-icon.png" />
                                   <h1>Ethereum</h1>
                               </div>
@@ -30,11 +27,13 @@ const Allow = () => {
                                     <img src="https://zapper.fi/images/networks/binance-smart-chain-icon.png" />
                                   <h1>BSC</h1>
                               </div>
-
-                          </div>
+                              <div className="item">
+                                    <img src="https://zapper.fi/images/networks/binance-smart-chain-icon.png" />
+                                  <h1>BSC</h1>
+                              </div>
                       </div>
-
                   </div>
+                  <div className="trip"></div>
 
               </div>
             <div className="item">
@@ -42,8 +41,8 @@ const Allow = () => {
                     <div className="no_flex">
                         <div className="item_empty">
                             <div className="item_empty_icon">
-                                <IoMdUnlock className='icon'/>
-                                <MdCancel className="cancle" size={40} color="red"/>
+                                <IoMdUnlock size={2.75} className='icon'/>
+                                <MdCancel className="cancle"  color="red"/>
                             </div>
                             <h1>No allowances found on Ethereum.</h1>
                         </div>
@@ -51,7 +50,6 @@ const Allow = () => {
                 </div>
             </div>
             </div>
-       </div>
    )
 }
 
